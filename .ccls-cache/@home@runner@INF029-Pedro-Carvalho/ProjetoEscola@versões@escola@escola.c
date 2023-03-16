de177935e20a2598dd92de0ec917d6098bce0e02
PedroSCarvalho <126918337+PedroHScarvalho3@users.noreply.github.com>
2023-03-16 16:14:13 +0000

    tentei adicionar um menu de listas

--- .ccls-cache/@home@runner@INF029-Pedro-Carvalho/ProjetoEscola@versões@escola@escola.c
+++ .ccls-cache/@home@runner@INF029-Pedro-Carvalho/ProjetoEscola@versões@escola@escola.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define limiteA 3
 #define limiteP 3
@@ -25,6 +26,10 @@ typedef struct ficha_pessoa{
 void invalido (){
   printf("*escolha invalida! escolha uma das opções do menu*\n");
 }
+void menulistas (){
+  printf("*como você quer listar?*\n");
+  printf("normalmente - 1\nalfabeticamente - 2\nem ordem de nascimento - 3\nsair - 0\n");
+}
 
 void menugeral (){
   printf("*em que area quer entrar?*\n");
@@ -38,9 +43,10 @@ void menualunos (){
 
 int main(){
 
-  int escolha,escolhaA, i, sair=0,sairA=0, limiteD=3, quantA=0, matricula,achou=0, j;
+  int escolha,escolhaA,escolha3, i, sair=0,sairA=0,sairL, limiteD=3, quantA=0, matricula,achou=0, j;
   dados aluno[limiteA], professor[limiteP];
-  int matriculanova, nomenovo, dianovo, mesnovo, anonovo, cpfnovo, sexonovo; 
+  int matriculanova, dianovo, mesnovo, anonovo, cpfnovo, sexonovo;
+  char nomenovo[50];
 
   while(sair!=1){
     menugeral();
@@ -56,6 +62,7 @@ int main(){
         while(sairA!=1){
           menualunos();
           scanf("%d",&escolhaA);
+          getchar();
           switch(escolhaA){
             case 0:{
               sairA=1;
@@ -74,7 +81,7 @@ int main(){
                 printf("digite o cpf do aluno %d:\n", i+1);
                 scanf("%d",&aluno[i].cpf);
                 getchar();
-                printf("digite o sexo do aluno %d:",i+1);
+                printf("digite o sexo do aluno %d:\n",i+1);
                 scanf("%c",&aluno[i].sexo);
                 getchar();
                 printf("digite a data de nascimento do aluno %d (no formato dd mm aa):\n", i+1);
@@ -91,22 +98,49 @@ int main(){
               break;
             }
             case 2:{
-              printf("*listar aluno*\n");
-              if(quantA==0){
-                printf("*sem cadastros disponiveis*\n");
-              }
-              else{
-                for(i=0;i<quantA;i++){
-                  if(aluno[i].ativo){
-                    printf("matricula do aluno %d: %d\n", i+1, aluno[i].matricula);
-                    printf("nome do aluno %d: %s",i+1, aluno[i].nome);
-                    printf("cpf do aluno %d: %d\n", i+1, aluno[i].cpf);
-                    printf("sexo do aluno %d: %c",i+1, aluno[i].sexo);
-                    printf("data de nascimento do aluno %d: %d/%d/%d\n", i+1, aluno[i].data.dia, aluno[i].data.mes, aluno[i].data.ano);
+              while(sairL!=1){
+                menulistas ();
+                scanf("%d",&escolha3);
+                getchar();
+                switch(escolha3){
+                  case 1:{
+                  printf("*listar aluno*\n");
+                  if(quantA==0){
+                  printf("*sem cadastros disponiveis*\n");
+                  }
+                  else{
+                  for(i=0;i<quantA;i++){
+                    if(aluno[i].ativo){
+                      printf("matricula do aluno %d: %d\n", i+1, aluno[i].matricula);
+                      printf("nome do aluno %d: %s",i+1, aluno[i].nome);
+                      printf("cpf do aluno %d: %d\n", i+1, aluno[i].cpf);
+                      printf("sexo do aluno %d: %c\n",i+1, aluno[i].sexo);
+                      printf("data de nascimento do aluno %d: %d/%d/%d\n", i+1, aluno[i].data.dia, aluno[i].data.mes, aluno[i].data.ano);
+                      }
+                    }
+                  }
+                  break;
+                  }
+                  case 2:{
+                    printf("*listar aluno por nome*\n");
+                    break;
+                  }
+                  case 3:{
+                    printf("&listar alunos por nascimento*\n");
+                    break;
+                  }
+                  case 0:{
+                    printf("*voltar*\n");
+                    sairL=1;
+                    break;
+                  }
+                  default:{
+                    invalido();
+                    break;
                   }
                 }
               }
-              break;
+            break;    
             }
             case 3:{
               printf("*atualizar aluno*\n");
@@ -122,13 +156,23 @@ int main(){
                   printf("*digite os novos dados\n*");
                   printf("qual será a nova matricula?\n");
                   scanf("%d",&matriculanova);
+                  getchar();
+                  printf("qual será o novo nome?");
+                  fgets(nomenovo,50,stdin);
+                  getchar();
                   printf("qual será o novo cpf?\n");
                   scanf("%d",&cpfnovo);
+                  getchar();
                   printf("qual será a nova data de nascimento? (no formato dd mm aa):\n");
                   scanf("%d",&dianovo);
+                  getchar();
                   scanf("%d",&mesnovo);
+                  getchar();
                   scanf("%d",&anonovo);
+                  getchar();
                   aluno[i].matricula=matriculanova;
+                  strcpy(aluno[i].nome,nomenovo);
+                  getchar();
                   aluno[i].cpf=cpfnovo;
                   aluno[i].data.dia=dianovo;
                   aluno[i].data.mes=mesnovo;
@@ -157,6 +201,7 @@ int main(){
                   aluno[i].ativo=-1;
                   for(j=i;j<quantA-1;j++){
                     aluno[j].matricula=aluno[j+1].matricula;
+                    strcpy(aluno[j].nome,aluno[j+1].nome);
                     aluno[j].cpf=aluno[j+1].cpf;
                     aluno[j].data.dia=aluno[j+1].data.dia;
                     aluno[j].data.mes=aluno[j+1].data.mes;
